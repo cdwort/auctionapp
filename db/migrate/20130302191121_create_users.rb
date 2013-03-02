e24e33794b5e805dbe92a757f3fab2352c9d87d3
Josh Zimmerman <jazimm137@gmail.com>
2013-03-02 13:36:19 -0600

    Added user_name and real_name fields to user db migration.

--- db/migrate/20130302191121_create_users.rb
+++ db/migrate/20130302191121_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :user_name
+      t.string :real_name
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130302182902) do
+ActiveRecord::Schema.define(version: 20130302191141) do
 
   create_table "auction_items", force: true do |t|
     t.string   "name"
-    t.datetime "endTime"
+    t.time     "endTime"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "bids", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
