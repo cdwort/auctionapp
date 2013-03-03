b5300c46506bd0ddcd028966e9804163bc229bbf
Josh Zimmerman <jazimm137@gmail.com>
2013-03-03 10:59:09 -0600

    Added stuff to db migrate.

--- db/migrate/20130302182902_create_auction_items.rb
+++ db/migrate/20130302182902_create_auction_items.rb
@@ -2,8 +2,9 @@ class CreateAuctionItems < ActiveRecord::Migration
   def change
     create_table :auction_items do |t|
       t.string :name
+      t.timestamp :start_time
       t.timestamp :end_time
-
+      t.integer :donor_id
       t.timestamps
     end
   end
