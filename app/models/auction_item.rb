eeebe877265b2dbfa713c7289e325a36beadb4e1
Josh Zimmerman <jazimm137@gmail.com>
2013-03-02 20:43:36 -0600

    Updated auction_item model with relations

--- app/models/auction_item.rb
+++ app/models/auction_item.rb
@@ -1,3 +1,5 @@
 class AuctionItem < ActiveRecord::Base
-has_many :bids
+  has_many :bids
+  has_many :bidding_users, :through => :bids, :class_name => "User", :uniq => true
+  belongs_to :donor, :class_name => "User"
 end
