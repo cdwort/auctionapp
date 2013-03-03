2ae8bb9bba675e4d464cc1c4375bd72cfaaba6c2
Josh Zimmerman <jazimm137@gmail.com>
2013-03-02 20:44:18 -0600

    Updated user model with relations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :donated_items, :class_name => "AuctionItem", :foreign_key => "donor_id"
+  has_many :bidded_items, :through => :bids, :class_name => "AuctionItem", :uniq => true
+  has_many :bids
 end
