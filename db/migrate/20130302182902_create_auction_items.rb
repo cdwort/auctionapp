4c41fcd94eb5fe6cf4c8e611ba5671d41a9d4898
Josh Zimmerman <jazimm137@gmail.com>
2013-03-02 21:05:45 -0600

    Fixing syntax on end_time in db migration for auction_item.

--- db/migrate/20130302182902_create_auction_items.rb
+++ db/migrate/20130302182902_create_auction_items.rb
@@ -2,7 +2,7 @@ class CreateAuctionItems < ActiveRecord::Migration
   def change
     create_table :auction_items do |t|
       t.string :name
-      t.timestamp :endTime
+      t.timestamp :end_time
 
       t.timestamps
     end
