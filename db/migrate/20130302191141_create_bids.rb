f21da82c87a42a0ab70de4fa4e0d589794b9f653
Josh Zimmerman <jazimm137@gmail.com>
2013-03-03 11:00:11 -0600

    Added to bids db migrate.

--- db/migrate/20130302191141_create_bids.rb
+++ db/migrate/20130302191141_create_bids.rb
@@ -1,7 +1,8 @@
 class CreateBids < ActiveRecord::Migration
   def change
     create_table :bids do |t|
-
+      t.integer :auction_item_id
+      t.integer :user_id
       t.timestamps
     end
   end
