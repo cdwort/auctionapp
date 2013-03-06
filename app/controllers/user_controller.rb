3e43e2fc322cd56900ed1067d935b8467883bb28
Amy Unger <aunger@library.wisc.edu>
2013-03-06 10:53:46 -0600

    set up strong parameters for devise

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,34 @@
 class UserController < ApplicationController
+  # attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html {redirect_to @user, notice : 'Your account was successfully created'}
+      else 
+        format.html {render action : "new"}
+      end
+    end
+  end
+
+  
+  def create
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(user_params)
+        format.html {redirect_to @user, notice : 'Your account was successfully modified'}
+      else 
+        format.html {render action : "edit"}
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params[:user].permit(:email, :password, :password_confirmation, :remember_me )
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :email, :password, :password_confirmation, :remember_me
+
   has_many :donated_items, :class_name => "AuctionItem", :foreign_key => "donor_id"
   has_many :bidded_items, :through => :bids, :class_name => "AuctionItem", :uniq => true
   has_many :bids
+
 end
